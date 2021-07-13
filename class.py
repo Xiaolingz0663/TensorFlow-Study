a6454e0c66c98a1dcf8212112e63bd76fe25d590
Xiaolingz0663 <74575707+Xiaolingz0663@users.noreply.github.com>
2021-07-13 14:03:55 -0400

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class Circle(object):
+    def _init_ (self, radius=3, color="blue"):
+        self.radius=radius
+        self.color=color
+    
+    def add_radius(self,r):
+        self.radius =self.radius + r
+        return (self.radius)
+
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle(0,0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()
